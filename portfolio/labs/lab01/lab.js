afc64c2a270029f6a90dfc18c59f561b295be1ff
justony <52521653+justony@users.noreply.github.com>
2019-07-05 17:24:49 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -1,18 +1,41 @@
-/*
 
-*/
+
+
 
 
 function start(e) {
+    var name = [
+        {
+            char: '蕭',
+            big5: 'BFBD',
+            cns:  '1-7227',
+            unicode: '856D'
+        },
+        {
+            char: '琮',
+            big5: 'DA7A',
+            cns:  '2-3D7E',
+            unicode: '742E'
+        },
+        {
+            char: '敬',
+            big5: 'B771',
+            cns:  '1-6439',
+            unicode: '656C'
+        }
+    ];
 
+    var ele = document.getElementById("div1");
+
+    for (var i = 0; i < 3; i++) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
 }
-
-/*
-https://www.w3schools.com/jsref/met_document_addeventlistener.asp
-
-document.addEventListener(event, function, useCapture)
-
-true - The event handler is executed in the capturing phase
-false- Default. The event handler is executed in the bubbling phase
-*/
-window.addEventListener( "load", start, false );
+window.addEventListener("load",start,false);
