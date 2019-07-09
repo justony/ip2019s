733982f4595444f0a9388d4fbc93bf09aa4033fd
justony <52521653+justony@users.noreply.github.com>
2019-07-09 17:54:54 +0800

    Add files via upload

--- portfolio/labs/lab04/lab.js
+++ portfolio/labs/lab04/lab.js
@@ -1,31 +1,20 @@
 $(function() {
-var card = ['ace-of-club.svg', 'ace-of-diamond.svg', 'ace-of-spades.svg'];
+var card = ['ace-of-club.svg', 'ace-of-diamonds.svg', 'ace-of-spades.svg', 'ace-of-hearts.svg'];
 
-for (var i =0; i < 16; i++)
-{
-  var r = Math.floor((Math.random() * 4));
-
-  $('#d2')
-.append('<img src="./' + card[r] + '">');
-}
-$('#b1').on('click', function() {
-
-    $('.issue').hide();
-    $('#d2').html('');
 
     for (var i = 0; i < 16; i++){
-        var r = Math.floor((Math.random() *4));
-        $('#d2').append('img class="issue"src=" src="./' + card[r] + '">');
+        var number = Math.floor((Math.random()*4));
+        $('#d2').append('<img class="issue" src="./' + card[number] + '">');
     }
     $('#b1').on('click', function(){
       $('.issue').hide();
-      $('#d2').html(' ');
-
+      $('#d2').html('');
       for (var i = 0; i < 16; i++){
-        $('#d2').append('img crc="./joker-card.svg">');
+        var r = Math.floor(Math.random()*4);
+
+        $('#d2').append('<img src="./joker-card.svg">');
       }
     });
-})
 });
 
 
