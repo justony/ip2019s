326055f56c1ab5a9b291e7217e2e8de10425b7be
justony <52521653+justony@users.noreply.github.com>
2019-07-11 18:04:55 +0800

    Add files via upload

--- portfolio/labs/lab07/lab.js
+++ portfolio/labs/lab07/lab.js
@@ -32,6 +32,10 @@ function analyze() {
       subs = remain.slice(m, n+1); // z 也要包含
       console.log('subs = ' + subs);
 
+      var subs2 = subs.replace('237','437');
+      var subs2 = subs2.replace('462','262');
+
+      var path = image.path(subs).fill('none').stroke({color:'red',width:'3px'}).draggable();
       remain = remain.slice(n+1); // z 也要移除
       //console.log('remain = ' + remain);
 
