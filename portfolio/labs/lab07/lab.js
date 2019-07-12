3b5852900ef5bf9f65518cc704141221cb464e3b
justony <52521653+justony@users.noreply.github.com>
2019-07-12 18:11:45 +0800

    Add files via upload

--- portfolio/labs/lab07/lab.js
+++ portfolio/labs/lab07/lab.js
@@ -15,7 +15,7 @@ function analyze() {
     do {
       i++;
 
-      m = remain.search("<path");
+      m = remain.search("<path>");
       console.log('m = ' + m);
       remain = remain.slice(m);
 
@@ -24,14 +24,15 @@ function analyze() {
       }
 
       m = remain.search('d=');
-      console.log('d = m ' + m);
-      remain = remain.slice(m);
-
-      m = remain.search(/m/i); // /z/i
-      console.log('m = ' + m);
+      console.log('m  m = ' + m);
+      remain = remain.search(m);
+      
+      m = remain.search(/m/i);
+      console.log('m   m = ' + m);
 
       n = remain.search(/z/i);
-      console.log('z  n = ' = n);
+      console.log('z  n = ' + n);
+
 
       subs = remain.slice(m, n+1); // z 也要包含
       console.log('subs = ' + subs);
@@ -40,45 +41,49 @@ function analyze() {
       var subs2 = subs2.replace('462','262');
 
       var path = image.path(subs).fill('black').stroke({color:'gray',width:5}).draggable();
+      
+      path.plot(sub2).draggable();
 
-      path.plot(subs2).draggable();
 
       var newPath = Snap.path.toCubic(subs);
-      console.log(' newPath.lengh = ' + newPath.lengh);
+      console.log(' newPath.length = ' + newPath.length);
 
 
       newPath.forEach(function(element){
-        console.log(element);
+        console.log(' newPath = ' + newPath.length);
       });
-
-      for (var i = 0; i< (newPath.length - 1); i++) {
-        for (var j = 0; j< (newPath[i].length); j++){
+      
+      for (var i =0; i < (newPath.length - 1); i++) {
+        for (var j = 0;j < newPath[i].length; j++) {
           console.log(' newPath[' + i + '][j] = ' + newPath[i][j] );
         }
 
 
-        if ( i == 0 ) { //M i==0
-          var circle = image.circle(20).fill('red').stroke('blue').move(newPath[i][1]-10, newPath[i][2]-10).draggable();
-        } else
-        var circle = image.circle(10).fill('pink').stroke('blue').move(newPath[i][1]-5, newPath[i][2]-5).draggable();
-        var circle = image.circle(10).fill('pink').stroke('blue').move(newPath[i][3]-5, newPath[i][4]-5).draggable();
-        var circle = image.circle(10).fill('pink').stroke('blue').move(newPath[i][5]-5, newPath[i][6]-5).draggable();
+        if ( i == 0 ) {
+          var circle = image.circle(20).fill('red').strock('blue').move(newPath[i][1]-10, newPath[i][2]-10).draggable();
+        }else {
+          var circle = image.circle(10).fill('pink').strock('blue').move(newPath[i][1]-5, newPath[i][2]-5).draggable();
+          var circle = image.circle(10).fill('pink').strock('blue').move(newPath[i][3]-5, newPath[i][4]-5).draggable();
+          var circle = image.circle(10).fill('pink').strock('blue').move(newPath[i][5]-5, newPath[i][6]-5).draggable();
         }
       }
 
+      //var pathString = "M382 371C440 281 80 162 82 314 84 467 324 462 382 371z"
+      //var newPath = Snap.path.toCubic(pathString);
 
+      for(var i = 0; i < newPath.length; i++){
+        var segment = newPath[i], point;
 
-
-
-
-      remain = remain.slice(n+1);
-
-
-
-
-
+        segment.shift();
+        point = setUpPoint(segment);
+      }
       var newPath = [];
       var move = '';
+
+      
+
+
+
 
       m = subs.search(/c/i);
       console.log('m = subs.search(/c/i);');
@@ -87,25 +92,27 @@ function analyze() {
       n = subs.search(/z/i);
       console.log('n = subs.search(/z/i);');
       console.log('n = ' + n);
-      
 
+
+      
+      var move = " ";
       move = subs.slice(1, m);
       console.log('move.length = ' + move.length);
       console.log('move = ' + move);
 
-      move = move.replace(',', ' ');
+      move = move.replace(',',' ');
       console.log('move.length = ' + move.length);
       console.log('move = ' + move);
+
 
       // https://stackoverflow.com/questions/40282519/split-string-by-multiple-spaces-nodejs
       var temp;
       temp = move.trim().split(/\s+/);
+      
       console.log('temp.length = ' + temp.length);
       console.log('temp = ' + temp);
       console.log('temp[0] = ' + temp[0]);
       console.log('temp[1] = ' + temp[1]);
-
-
 
       var x, y;
       x = parseInt(temp[0],10);
@@ -115,12 +122,7 @@ function analyze() {
 
       var circle = image.circle(20).fill('red').stroke('blue').move(x-10, y-10).draggable();
 
-      /*
-      temp.forEach(function(element) {
-        newPath.push(element);
-      });
-      */
-
+      var newPath = [];
       console.log('newPath = ' + newPath);
 
       subs = subs.slice(m+1, n);  // C 也要移除 (大寫C), z 也要移除 
@@ -138,8 +140,7 @@ function analyze() {
 
       //var newPath = Snap.path.toCubic(subs);
 
-      console.log(' newPath.length = ' +  'newPath.length);
-      console.log(' m = ' +m);
+      console.log(' newPath.length = ' + newPath.length);
 
       /*
       //for(var i = 0; i < newPath.length; i++){
@@ -155,11 +156,11 @@ function analyze() {
     } while (m > 0);
 }
 
-  function saveText(text, filename) {
-    var a = document.createElement('a');
-    a.setAttribute('href', 'data:text/plain;charset=utf-u,' + encodeURIComponent(text));
-    a.setAttribute('download', filename);
-    a.click()
+function saveText(text, filename) {
+  var a = document.createElement('a');
+  a.setAttribute('href', 'data:text/plain;charset=utf-u,' + encodeURIComponent(text));
+  a.setAttribute('download', filename);
+  a.click()
 }
 
 function readFiles(files){
@@ -217,6 +218,23 @@ function handleDragOver(evt) {
 
 function start(e) {
 
+  $(document).mousemove(function(event){
+    var p = $( "#svgimage2" );
+
+    var position = p.position();
+
+    console.log('position = ' + position.left + ',' + position.top);
+
+    var myX = event.pageX - Math.round(position.left);
+    var myY = event.pageY - Math.round(position.top) + 500;
+
+    $("#s5").html("<div style='position:absolute; border-style:none; TOP:"
+        + event.pageY + "px; LEFT:"
+        + event.pageX + "px;'>" + "&nbsp&nbsp&nbsp&nbsp("
+        + myX + ", "
+        + myY + ")"
+        + "</div>");
+  });
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
   var dropZone = document.getElementById('drop_zone');
